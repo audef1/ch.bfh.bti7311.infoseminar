e911c73a86acad55d39812abd982d09c677c91e7
Auderset Florian <florian.auderset@staempfli.com>
2018-04-23 18:26:35 +0200

    Initial.

---
+++ STUN/index.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+    include_once("STUNClient.php");
+
+    error_reporting(E_ERROR);
+
+    $client = new STUNClient();
+    $client->setServerAddr("stun.stunprotocol.org");
+    $client->createSocket();
+
+    print("NAT TYPE:" . $client->natType2String($client->getNatType()) ."\n");
+    print("MAPPED ADDRESS:" . $client->getMappedAddr() . "\n");
